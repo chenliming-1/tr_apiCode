98ce7f92e68d09bc9c69f5e73091ed3afc980563
龚远琪 <gongyq@histudy.com>
2019-07-26 09:55:39 +0800

    添加题型脚本新增用例

--- data/teacher/itemtype/__init__.py
+++ data/teacher/itemtype/__init__.py
@@ -1,8 +1,8 @@
 # #!Date：2019/7/9 11:28
 # !@Author：龚远琪
 #
-
 from .additemtype import additemtype
 from .deleteitemtype import deleteitemtype
+from .getitemtype import getItemType
 
-__all__ = ['additemtype', 'deleteitemtype']
+__all__ = ['additemtype', 'deleteitemtype', 'getItemType']
---
+++ data/teacher/itemtype/getitemtype.py
@@ -0,0 +1,15 @@
+# #!Date：2019/7/25 9:54
+# !@Author：龚远琪
+#
+from common.commonmethod import *
+
+getItemType = {
+    "url": f'{sysurl}tr/api/tr/item-types/',
+
+    "header": {
+        "Cookie": cookie,
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+}
